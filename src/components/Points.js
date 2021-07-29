768b88b3e6c28b18f198c0cb2187cfe38bfa2e6d
daniel70125 <dwrighttt504@gmail.com>
2021-07-30 03:21:32 +0800

    added total to bottom of points.js 2/2

--- src/components/Points.js
+++ src/components/Points.js
@@ -18,6 +18,7 @@ const Points = (props) => {
     console.log(finalPoints)
     return ( 
         <div id='points-wrapper'>
+            <p id='points-main-p'>{props.elm.name} has {finalPoints} points available !</p>
             <p>Transactions: </p>
                 <ul>
                 {user}
@@ -26,11 +27,14 @@ const Points = (props) => {
                 <span className='points-span-total'>Total: ${Math.floor(totalSpent)}</span>
             <p className='points-rules'>2 Points for each dollar spent over $100</p>
                 <span>${Math.floor(totalSpent)} - $100 = {Math.floor(totalSpent) >= 100 ? Math.floor(totalSpent) - 100 : 0}</span> <br />
-                <span>{Math.floor(totalSpent) >= 100 ?  Math.floor(totalSpent) - 100 : 0} x 2 points = {Math.floor(totalSpent) >= 100 ? userPoints * 2 : 0} points</span>
+                <span>{Math.floor(totalSpent) >= 100 ?  Math.floor(totalSpent) - 100 : 0} x 2 points = {Math.floor(totalSpent) >= 100 ? (Math.floor(totalSpent) - 100) * 2 : 0} points</span>
             <p className='points-rules'>1 Point for each dollar spent over $50</p>
                 <span>${Math.floor(totalSpent) >= 100 ? 100 : Math.floor(totalSpent)} - $50 = {Math.floor(totalSpent) >= 100 ? 100 - 50  : Math.floor(totalSpent) < 100 && Math.floor(totalSpent) >= 50 ? Math.floor(totalSpent) - 50 : 0 }</span> <br />
                 <span>{Math.floor(totalSpent) >= 100 ? 100 - 50 : Math.floor(totalSpent) < 100 && Math.floor(totalSpent) > 50 ? Math.floor(totalSpent) - 50  : 0} x 1 points = {Math.floor(totalSpent) >= 100 ? 100 - 50 : Math.floor(totalSpent) < 100 && Math.floor(totalSpent) > 50 ? Math.floor(totalSpent) - 50 : 0  } points</span>
-            <p>{props.elm.name} has {finalPoints} points available !</p>
+                <br />
+                <br />
+                <p className='points-rules'>Total: </p>
+                <span>{over100Points} + {over50Points} = {finalPoints} total points</span>
         </div>
      );
 }
